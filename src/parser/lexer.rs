c87ed198e3a0eb9a7279b19a70e48ecc883f06d0
GalileoCap <galileocapp@gmail.com>
2023-12-18 20:06:52 -0300

    OK: Tokenize identifiers

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -27,10 +27,16 @@ fn get_number(c : char, chars : &mut Chars) -> LexerRes<Token> {
     Ok(Token::Number(c.to_string() + &chars.take_while(|c| c.is_digit(10)).collect::<String>()))
 }
 
+fn get_ident(c : char, chars : &mut Chars) -> LexerRes<Token> {
+    Ok(Token::Identifier(c.to_string() + &chars.take_while(|c| c.is_alphanumeric() || c == &'_').collect::<String>()))
+}
+
 pub fn gettok(chars : &mut Chars) -> LexerRes<Token> {
     let c = skip_whitespace(chars)?;
     if c.is_digit(10) {
         return get_number(c, chars)
+    } else if c.is_alphabetic() {
+        return get_ident(c, chars)
     } else {
         todo!("")
     }
@@ -41,4 +47,13 @@ fn test_get_number() {
     assert_eq!(gettok(&mut "0".chars()), Ok(Token::Number("0".to_string())));
     assert_eq!(gettok(&mut " 1".chars()), Ok(Token::Number("1".to_string())));
     assert_eq!(gettok(&mut "2  3".chars()), Ok(Token::Number("2".to_string())));
+}
+
+#[test]
+fn test_get_identifier() {
+    assert_eq!(gettok(&mut "g".chars()), Ok(Token::Identifier("g".to_string())));
+    assert_eq!(gettok(&mut "galileo".chars()), Ok(Token::Identifier("galileo".to_string())));
+    assert_eq!(gettok(&mut "gali leo".chars()), Ok(Token::Identifier("gali".to_string())));
+    assert_eq!(gettok(&mut "g4l1l30".chars()), Ok(Token::Identifier("g4l1l30".to_string())));
+    assert_eq!(gettok(&mut "gali_leo".chars()), Ok(Token::Identifier("gali_leo".to_string())));
 }
