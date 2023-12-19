be91d63c8bae189837648c9975f9f7f5d886ab4e
GalileoCap <galileocapp@gmail.com>
2023-12-19 10:33:10 -0300

    OK: Added SContext

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
 use crate::parser::{Expr, parse_str};
 use crate::utils::{SError, SRes};
 
@@ -19,77 +22,87 @@ impl SValue {
     }
 }
 
-fn execute_none() -> SRes<SValue> {
+pub struct SContext {
+}
+
+impl SContext {
+    fn new() -> SContext {
+        SContext{
+        }
+    }
+}
+
+fn execute_none(ctx : &mut SContext) -> SRes<SValue> {
     Ok(SValue::None)
 }
 
-fn execute_number(x : i32) -> SRes<SValue> {
+fn execute_number(x : i32, ctx : &mut SContext) -> SRes<SValue> {
     Ok(SValue::Number(x))
 }
 
-fn execute_bool(value : bool) -> SRes<SValue> {
+fn execute_bool(value : bool, ctx : &mut SContext) -> SRes<SValue> {
     Ok(SValue::Bool(value))
 }
 
-fn execute_block(exprs : &Vec<Expr>) -> SRes<SValue> {
-    exprs.iter().fold(Ok(SValue::None), |_, e| Ok(execute(e)?))
+fn execute_block(exprs : &Vec<Expr>, ctx : &mut SContext) -> SRes<SValue> {
+    exprs.iter().fold(Ok(SValue::None), |_, e| Ok(execute(e, ctx)?))
 }
 
-fn execute_add(lhs : &Box<Expr>, rhs : &Box<Expr>) -> SRes<SValue> {
-    let SValue::Number(l) = execute(lhs)?.to_number()? else { panic!() };
-    let SValue::Number(r) = execute(rhs)?.to_number()? else { panic!() };
+fn execute_add(lhs : &Box<Expr>, rhs : &Box<Expr>, ctx : &mut SContext) -> SRes<SValue> {
+    let SValue::Number(l) = execute(lhs, ctx)?.to_number()? else { panic!() };
+    let SValue::Number(r) = execute(rhs, ctx)?.to_number()? else { panic!() };
     Ok(SValue::Number(l + r))
 }
 
-fn execute_binary_op(op : &String, lhs : &Box<Expr>, rhs : &Box<Expr>) -> SRes<SValue> {
+fn execute_binary_op(op : &String, lhs : &Box<Expr>, rhs : &Box<Expr>, ctx : &mut SContext) -> SRes<SValue> {
     match &**op { // TODO: Call op on lhs with rhs
-        "+" => execute_add(lhs, rhs),
+        "+" => execute_add(lhs, rhs, ctx),
         _ => todo!(), // TODO: Custom binary ops
     }
 }
 
-pub fn execute(e : &Expr) -> SRes<SValue> {
+pub fn execute(e : &Expr, ctx : &mut SContext) -> SRes<SValue> {
     match e {
-        Expr::None => execute_none(),
-        Expr::Number(x) => execute_number(*x),
-        Expr::Bool(value) => execute_bool(*value),
-        Expr::Block(exprs) => execute_block(exprs),
-        Expr::BinaryOp { op, lhs, rhs } => execute_binary_op(op, lhs, rhs),
+        Expr::None => execute_none(ctx),
+        Expr::Number(x) => execute_number(*x, ctx),
+        Expr::Bool(value) => execute_bool(*value, ctx),
+        Expr::Block(exprs) => execute_block(exprs, ctx),
+        Expr::BinaryOp { op, lhs, rhs } => execute_binary_op(op, lhs, rhs, ctx),
         _ => todo!(),
     }
 }
 
-pub fn execute_str(s : &str) -> SRes<SValue> {
-    execute(&parse_str(s)?)
+pub fn execute_str(s : &str, ctx : &mut SContext) -> SRes<SValue> {
+    execute(&parse_str(s)?, ctx)
 }
 
 #[test]
 fn test_none() {
-    assert_eq!(execute_str("none"), Ok(SValue::None));
+    assert_eq!(execute_str("none", &mut SContext::new()), Ok(SValue::None));
 }
 
 #[test]
 fn test_number() {
-    assert_eq!(execute_str("0"), Ok(SValue::Number(0)));
-    assert_eq!(execute_str("1"), Ok(SValue::Number(1)));
+    assert_eq!(execute_str("0", &mut SContext::new()), Ok(SValue::Number(0)));
+    assert_eq!(execute_str("1", &mut SContext::new()), Ok(SValue::Number(1)));
 }
 
 #[test]
 fn test_bool() {
-    assert_eq!(execute_str("true"), Ok(SValue::Bool(true)));
-    assert_eq!(execute_str("false"), Ok(SValue::Bool(false)));
+    assert_eq!(execute_str("true", &mut SContext::new()), Ok(SValue::Bool(true)));
+    assert_eq!(execute_str("false", &mut SContext::new()), Ok(SValue::Bool(false)));
 }
 
 #[test]
 fn test_block() {
-    assert_eq!(execute_str("{}"), Ok(SValue::None));
-    assert_eq!(execute_str("{0}"), Ok(SValue::Number(0)));
-    assert_eq!(execute_str("{0 1}"), Ok(SValue::Number(1)));
+    assert_eq!(execute_str("{}", &mut SContext::new()), Ok(SValue::None));
+    assert_eq!(execute_str("{0}", &mut SContext::new()), Ok(SValue::Number(0)));
+    assert_eq!(execute_str("{0 1}", &mut SContext::new()), Ok(SValue::Number(1)));
 }
 
 #[test]
 fn test_binary_op() {
-    assert_eq!(execute_str("1 + 2"), Ok(SValue::Number(3)));
+    assert_eq!(execute_str("1 + 2", &mut SContext::new()), Ok(SValue::Number(3)));
     //assert_eq!(execute_str("1 - 2"), Ok(SValue::Number(-1)));
     //assert_eq!(execute_str("1 * 2"), Ok(SValue::Number(2)));
     //assert_eq!(execute_str("1 / 2"), Ok(SValue::Number(1 / 2))); // TODO: Floating point
