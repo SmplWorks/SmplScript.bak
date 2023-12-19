8d5ccfc7760536f97b9c269e4fdcb70463008c47
GalileoCap <galileocapp@gmail.com>
2023-12-18 23:13:32 -0300

    OK: Parse block

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -5,21 +5,48 @@ use super::*;
 #[derive(Debug, PartialEq)]
 pub enum Expr {
     Number(i32),
+    Block(Vec<Expr>),
 }
 
-fn gettok(toks : &mut Peekable<Tokens>) -> ParserRes<Token> {
+fn peektok(toks : &mut Peekable<Tokens>) -> ParserRes<Token> {
     Ok(toks.peek().ok_or(ParserError::EOF)?.clone())
+}
+
+fn nexttok(toks : &mut Peekable<Tokens>) -> ParserRes<Token> {
+    Ok(toks.next().ok_or(ParserError::EOF)?.clone())
+}
+
+fn collect_while(toks : &mut Peekable<Tokens>, f : impl Fn(&Token) -> bool) -> ParserRes<Vec<Expr>> {
+    let mut exprs = vec![];
+    loop {
+        let t = nexttok(toks)?;
+        if f(&t) {
+            break;
+        }
+
+        exprs.push(parse_tok(t, toks)?);
+    }
+    return Ok(exprs);
 }
 
 fn parse_number(s : &String, _toks : &mut Peekable<Tokens>) -> ParserRes<Expr> {
     Ok(Expr::Number(s.parse().map_err(|_| ParserError::InvalidNumber)?))
 }
 
-pub fn parse(toks : &mut Peekable<Tokens>) -> ParserRes<Expr> {
-    match gettok(toks)? {
+fn parse_block(toks : &mut Peekable<Tokens>) -> ParserRes<Expr> {
+    Ok(Expr::Block(collect_while(toks, |t| *t == Token::RBrack)?))
+}
+
+fn parse_tok(t : Token, toks : &mut Peekable<Tokens>) -> ParserRes<Expr> {
+    match t {
         Token::Number(s) => parse_number(&s, toks),
-        _ => todo!(),
+        Token::LBrack => parse_block(toks),
+        _ => todo!("{:?}", peektok(toks)),
     }
+}
+
+pub fn parse(toks : &mut Peekable<Tokens>) -> ParserRes<Expr> {
+    parse_tok(nexttok(toks)?, toks)
 }
 
 pub fn parse_str(s : &str) -> ParserRes<Expr> {
@@ -33,4 +60,11 @@ pub fn parse_string(s : &String) -> ParserRes<Expr> {
 #[test]
 fn test_parse_number() {
     assert_eq!(parse_str("0"), Ok(Expr::Number(0)));
+}
+
+#[test]
+fn test_parse_block() {
+    assert_eq!(parse_str("{}"), Ok(Expr::Block(vec![])));
+    assert_eq!(parse_str("{0}"), Ok(Expr::Block(vec![Expr::Number(0)])));
+    assert_eq!(parse_str("{{0}}"), Ok(Expr::Block(vec![Expr::Block(vec![Expr::Number(0)])])));
 }
